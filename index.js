0fc6f172cdea964d730f37ebc46764bb8267ad71
athenamw <wallisathena@gmail.com>
2023-07-04 20:23:31 -0700

    wrote questions and function

--- index.js
+++ index.js
@@ -39,10 +39,54 @@ const questions = () => {
 };
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+const writeToFile = ({ title, description, installation, usage, contributions, test }) =>
+  `<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <header id="title">${title}</header>
+        <section id="description">
+            <h1> DESCRIPTION </h1>
+            <p>${description}</p>
+        </section>
+        <section id="toc">
+            <h1> TABLE OF CONTENTS </h1>
+        </section>
+        <section id="installation>
+            <h1> INSTALLATION </h1>
+            <p>${installation}</p>
+        <section id="usage">
+            <h1>USAGE</h1>
+            <p>${usage}</p>
+        </section>
+        <section id="license">
+            <h1>LICENSE</h1>
+            <p></p>
+        </section>
+        <section id="contribute">
+            <h1>CONTRIBUTING</h1>
+            <p>${contributions}</p>
+        </section>
+        <section id="tests">
+            <h1>TESTS</h1>
+            <p>${test}</p>
+        </section>
+        <section id="question">
+            <h1>QUESTIONS</h1>
+            <p></p>
+        
+        
+           Questions
+    
+</body>
+</html>`;
 
 // TODO: Create a function to initialize app
-const init = () => {
+const initialize = () => {
   questions()
     .then((responses) => writeFile('index.html', generateHTML(responses)))
     .then(() => console.log('correct'))
@@ -50,4 +94,4 @@ const init = () => {
 };
 
 // Function call to initialize app
-init();
+initialize();
