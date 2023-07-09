113d5b84b9e3399318142c1433e559050685c71b
athenamw <wallisathena@gmail.com>
2023-07-08 21:16:09 -0700

    trying to correct mistakes. rewrite index/markdown

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
-const file = require('fs').promises;
-import { generateMarkdown, renderLicenseBadge } from './utils/generateMarkdowns.js';
-let licenceBadge = renderLicenseBadge(licence);
-// markdown = require('./utils/generateMarkdowns.js');
+const file = require('fs');
+const generateMarkdown = require('./utils/generateMarkdowns');
 
 // TODO: Create an array of questions for user input
 const questions = () => {
@@ -12,11 +10,27 @@ const questions = () => {
       type: 'input',
       name: 'title',
       message: 'What is the title of your project?',
+      validate: titleInput => {
+        if (titleInput){
+          return true;
+        }else{
+          console.log('Please enter a valid title for your project');
+          return false;
+        }
+      }
     },
     {
       type: 'input',
       name: 'description',
       message: 'Please describe your project.',
+      validate: titleInput => {
+        if (descriptionInput){
+          return true;
+        }else{
+          console.log('Please enter a valid description for your project');
+          return false;
+        }
+      }
     },
     {
       type: 'input',
@@ -59,68 +73,26 @@ const questions = () => {
 };
 
 // TODO: Create a function to write README file
-const writeToFile = ({ title, description, installation, usage, license, contributions, test }) =>
-  `<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script src="https://cdn.tailwindcss.com"></script>
-    <title>Tell Me The Facts</title>
-</head>
-<body>
-    <header id="title" class="text-7xl text-center">${title}</header>
-    <img src="${licenseBadge(license)}">
-        <section id="description" class="content-between">
-            <h1 class="text-4xl"> DESCRIPTION </h1>
-            <p>${description}</p>
-        </section>
-        <section id="toc">
-            <h1 class="text-4xl"> TABLE OF CONTENTS </h1>
-            <ol>
-            <li><a href="#description">Description</a></li>
-            <li><a href="#installation">Installation</a></li>
-            <li><a href="#usage">Usage<a/></li>
-            <li><a href="#license">License</a></li>
-            <li><a href="#contribute">Contributing</a></li>
-            <li><a href="#tests">Tests</a></li>
-            <li><a href="#question"><Questions></a></li>
-            </ol>
-        </section>
-        <section id="installation">
-            <h1 class="text-4xl"> INSTALLATION </h1>
-            <p>${installation}</p>
-        <section id="usage">
-            <h1 class="text-4xl">USAGE</h1>
-            <p>${usage}</p>
-        </section>
-        <section id="license">
-            <h1 class="text-4xl">LICENSE</h1>
-            <p>${license}</p>
-        </section>
-        <section id="contribute">
-            <h1 class="text-4xl">CONTRIBUTING</h1>
-            <p>${contributions}</p>
-        </section>
-        <section id="tests">
-            <h1 class="text-4xl">TESTS</h1>
-            <p>${test}</p>
-        </section>
-        <section id="question">
-            <h1 class="text-4xl">QUESTIONS</h1>
-            <p></p>
-        </section>
-    
-</body>
-</html>`;
+const writeToFile = (fileName, data) {
+  fs.writeToFile('./dist/README.md', generateMarkdown(data), function(err){
+    if(err){
+      return console.log(err);
+    }
+    console.log('Your file has been created successfully!');
+  });
+}
 
 // TODO: Create a function to initialize app
-const initialize = () => {
-  questions()
-    .then((responses) => file.writeFile('index.html', writeToFile(responses)))
-    .then(() => console.log('File Generated Successfully!'))
-    .catch((err) => console.error(err));
-};
+function initialize(){
+  inquirer.prompt(questions)
+  .then(function(answer){
+    const fileName = answer.title
+    .split(' ')
+    .join('') + '.md';
+
+    writeToFile(fileName.answer);
+  });
+}
 
 // Function call to initialize app
 initialize();
--- utils/generateMarkdowns.js
+++ utils/generateMarkdowns.js
@@ -1,23 +1,46 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
-  const apacheBadge = 'https://img.shields.io/badge/License-Apache_2.0-blue.svg';
-  const mitBadge = 'https://img.shields.io/badge/License-MIT-yellow.svg';
-  const artisticBadge = 'https://img.shields.io/badge/License-Artistic_2.0-0298c3.svg';
-  const gplBadge = 'https://img.shields.io/badge/License-GPLv3-blue.svg';
-
   if (license == 'Gnu GPL v3') {
-    return gplBadge;
+    return `[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)`;
+  } else if (license == 'MIT') {
+    return `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`;
+  } else if (license == 'apache-2.0') {
+    return `[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`;
+  } else if (license == 'artistic-2.0') {
+    return `[![License: Artistic-2.0](https://img.shields.io/badge/License-Artistic_2.0-0298c3.svg)](https://opensource.org/licenses/Artistic-2.0)`;
+  } else if (license.length === 0) {
+    return ``;
   }
 }
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license == 'Gnu GPL v3') {
+    return `https://www.gnu.org/licenses/gpl-3.0`;
+  } else if (license == 'MIT') {
+    return `https://opensource.org/licenses/MIT`;
+  } else if (license == 'apache-2.0') {
+    return `https://opensource.org/licenses/Apache-2.0`;
+  } else if (license == 'artistic-2.0') {
+    return `https://opensource.org/licenses/Artistic-2.0`;
+  } else if (license.length === 0) {
+    return '';
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license.length === 0) {
+    return '';
+  } else {
+    return `## License
+    ${renderLicenseBadge(license)}
+    ${renderLicenseLink(license)}`;
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
@@ -27,11 +50,3 @@ function generateMarkdown(data) {
 }
 
 module.exports.generateMarkdown = generateMarkdown;
-
-// [![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)
-
-// [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
-
-// [![License: Artistic-2.0](https://img.shields.io/badge/License-Artistic_2.0-0298c3.svg)](https://opensource.org/licenses/Artistic-2.0)
-
-// [![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)
